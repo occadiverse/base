35f95171ec6e493bbca289b7085317d8f63e84a0
occadiverse <occadiverse@gmail.com>
2026-05-19 14:00:31 +0200

    Update firestore-config.js

--- firestore-config.js
+++ firestore-config.js
@@ -1,4 +1,4 @@
-// firebase-config.js
+// firestore-config.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-firestore.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-auth.js";
