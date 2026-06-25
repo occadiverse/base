eecddf4f38850e799f7ef2dcbbbff9c1167e98bb
occadiverse <occadiverse@gmail.com>
2026-06-25 20:05:53 +0200

    just 6 og 8

--- assets/js/pages/matches.js
+++ assets/js/pages/matches.js
@@ -405,9 +405,9 @@ const matchGamePlanOffCPositions = {
     3: { top: '7%', left: '50%', tone: 'green' },
     4: { top: '7%', left: '42%', tone: 'green' },
     5: { top: '3%', left: '63%', tone: 'green' },
-    6: { top: '16%', left: '78%', tone: 'yellow' },
+    6: { top: '17%', left: '75%', tone: 'yellow' },
     7: { top: '12%', left: '50%', tone: 'yellow' },
-    8: { top: '16%', left: '22%', tone: 'yellow' },
+    8: { top: '17%', left: '25%', tone: 'yellow' },
     9: { top: '41%', left: '50%', tone: 'pink' },
     10: { top: '50%', left: '50%', tone: 'pink' }
 };
